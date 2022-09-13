0f6e9053b6be805b03e3c4ecc4ba78d42e9a431e
Lidervil <george.villarroel@outlook.es>
2022-09-13 10:24:35 -0300

    Complete ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringi, numb) {
 
+    if (numb < 0){
+        return 'ERROR'
+    }
+    let str = stringi;
+    let num = numb;
+    let result = "";
+    for(let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
